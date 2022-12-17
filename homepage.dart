d24fb1d2e1f484eda19e7d251a126da9dbd55154
AnthRexx11 <112256193+Anthrexx@users.noreply.github.com>
2022-12-17 16:57:54 +0530

    Update homepage.dart

--- homepage.dart
+++ homepage.dart
@@ -1,27 +1,21 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_application_1/pages/homepage.dart';
-import 'package:flutter_application_1/pages/login_page.dart';
 
-void main() {
-  runApp(Myapp());
-}
-
-class Myapp extends StatelessWidget {
-  const Myapp({super.key});
-
+class homepage extends StatelessWidget {
+  const homepage({super.key});
+  // String s = "KYNTRA";
   @override
   Widget build(BuildContext context) {
-    // main code yha se suru hota hai phle material app  ka use karke usme home
-    // bnate hai then usme child child karke chizo ko use karte hain.
-    return MaterialApp(
-      // home: homepage(),
-      // themeMode: ThemeMode.dark,
-      themeMode: ThemeMode.light,
-      theme: ThemeData(primarySwatch: Colors.deepPurple),
-      routes: {
-        "/": (context) => homepage(),
-        "/login_page": (context) => login_page()
-      },
+    return Scaffold(
+      appBar: AppBar(title: Text("KYNTRA")),
+      body: Material(
+        child: Center(
+          // text aling to centre
+          child: Container(
+            child: Text("WELCOME TO KYNTRA"),
+          ),
+        ),
+      ),
+      drawer: Drawer(),
     );
   }
 }
